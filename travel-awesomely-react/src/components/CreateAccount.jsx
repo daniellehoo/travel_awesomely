7a25cd023e6a044e8edef5dcb48d84276660582f
David <david@getoperatr.com>
2018-05-09 15:42:51 -0400

    duplicate render CreateAccount.js

--- travel-awesomely-react/src/components/CreateAccount.jsx
+++ travel-awesomely-react/src/components/CreateAccount.jsx
@@ -46,44 +46,6 @@ class CreateAccount extends Component {
               </div>
           )
       }
-
-    render(){
-        return(
-    <div className="login">
-        <Nav />
-        <form>
-        <h1>Sign Up</h1>
-        <p>Please fill in this form to create an account.</p>
-            <label>Name
-            <br/>
-                <input type="text" name="name" />
-            </label>
-        <br />
-        <br />
-            <label>Username
-            <br/>
-                <input type="text" name="username" />
-            </label>
-        <br />
-        <br />
-            <label>Password
-            <br/>
-                <input type="password" name="password" />
-            </label>
-        <br />
-        <br />
-            <label>Email
-            <br/>
-                <input type="email" name="email" />
-            </label>
-        <br />
-        <br />
-            <input type="submit" value="Submit" />
-        </form>
-            <Footer />
-            </div>
-        )
-    }
 }
 
 export default CreateAccount;
