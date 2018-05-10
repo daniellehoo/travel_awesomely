f7c87afd4a556eabb246e9438f492f9a08cde4bf
David <davidclelland6@gmail.com>
2018-05-10 08:43:47 -0400

    updated css

--- client/src/components/Countries.jsx
+++ client/src/components/Countries.jsx
@@ -6,7 +6,6 @@ class Countries extends Component {
     render(){
         return(
             <div className="landing">
-                <h1>Countries</h1>
             <Nav />
                 <h1>Countries</h1>
                 <h3>Hi David, choose a country</h3>
