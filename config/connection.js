05cbfcde74a9860081e32081f3c2797b6160d666
Danielle Hoo <daniellem.hoo@gmail.com>
2018-05-15 16:12:01 -0400

    deploying master to heroku

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ const pg = require('pg-promise')({
 const config = {
   host:     'localhost',
   port:     5432,
-  database: 'travel_db',
+  // database: 'travel_db',
 };
-// const db = pg(process.env.DATABASE_URL || config);
+const db = pg(process.env.DATABASE_URL || config);
 const db = pg(config);
 module.exports = db;
 // for heroku deployment: uncomment first line and comment line 9
